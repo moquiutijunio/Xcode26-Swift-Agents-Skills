e243115d31282b27c9ebe12dbe3d80447c10c826
Junio Cesar Moquiuti <idd_jmoquiuti@inv-tkq696m49j.uolcorp.intranet>
2026-04-10 17:49:49 -0400

    first commit

---
+++ swift-agents-skills/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-agents-skills
+//
+//  Created by Junio Cesar Moquiuti on 10/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-agents-skills/swift_agents_skillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_agents_skillsApp.swift
+//  swift-agents-skills
+//
+//  Created by Junio Cesar Moquiuti on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_agents_skillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
